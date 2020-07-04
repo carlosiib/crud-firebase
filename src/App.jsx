3e79830a6c158c12443bff7d5a1c39e25f758acc
carlosiib <ntic.carlos@gmail.com>
2020-07-04 12:07:55 -0700

    Form handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React, { useEffect, useState } from "react";
 import { firebase } from "./firebase";
 
 function App() {
+  //DB state
   const [tareas, setTareas] = useState([]);
+  //form state
+  const [tarea, setTarea] = useState("");
 
   useEffect(() => {
     const obtenerDatos = async () => {
@@ -28,6 +31,17 @@ function App() {
     obtenerDatos();
   }, []);
 
+  //form handler
+  const agregar = async (e) => {
+    e.preventDefault();
+
+    if (!tarea.trim()) {
+      console.log("Ingrese datos");
+      return;
+    }
+    console.log(tarea);
+  };
+
   return (
     <div className="container mt-3">
       <div className="row">
@@ -44,7 +58,24 @@ function App() {
             ))}
           </ul>
         </div>
-        <div className="col-md-6">Formulario</div>
+        <div className="col-md-6">
+          <h3>Formulario</h3>
+          <form onSubmit={agregar}>
+            <input
+              type="text"
+              placeholder="Ingrese Tarea"
+              className="form-control mb-2 "
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button
+              className="btn btn-dark btn-block"
+              type="submit"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
