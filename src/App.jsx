7d6418639aa821b18aab36c3084f6d036ced35fd
carlosiib <ntic.carlos@gmail.com>
2020-07-04 11:35:24 -0700

    DB Connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { firebase } from "./firebase";
 
 function App() {
-  return <div className="container">working</div>;
+  const [tareas, setTareas] = useState([]);
+
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection("tareas").get();
+        console.log(data.docs);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerDatos();
+  }, []);
+
+  return <div className="container">Working</div>;
 }
 
 export default App;
